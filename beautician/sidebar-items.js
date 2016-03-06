db8e0c9416ae872ebc594e298b9183372bf55434
Andrew Roetker <andrew.roetker@puppetlabs.com>
2016-03-06 15:31:10 -0800

    Add some documentation examples

--- beautician/sidebar-items.js
+++ beautician/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["escape_bytes",""],["escape_str","Escapes the given string into the writer."],["prettify","Prettifies the JSON content from the reader into the writer."]],"struct":[["Prettifier",""]]});
+initSidebarItems({"fn":[["escape_bytes",""],["escape_str","Escapes the given string into the writer."],["prettify","Prettifies the JSON content from the reader into the writer. # Examples"]],"struct":[["Prettifier",""]]});
